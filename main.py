50fae7588f8c4dff89a7dc2281bb77b07b5f7481
vodaman <volodymyr_ua@outlook.com>
2024-11-04 09:52:10 +0100

    Edited navbar, added 404 page

--- main.py
+++ main.py
@@ -6,4 +6,8 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('not_found.html')
+
 app.run(debug=True)
