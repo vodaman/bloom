6a03183252d0094be83ae463050d4db6732af9de
vodaman <volodymyr_ua@outlook.com>
2024-11-28 18:56:20 +0100

    edited a bunch of stuff

--- main.py
+++ main.py
@@ -10,4 +10,18 @@ def home():
 def not_found(e):
     return render_template('not_found.html')
 
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+@app.route("/login")
+def login():
+    return render_template("login.html")
+@app.route("/forgot")
+def forgot():
+    return render_template("forgot.html")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
 app.run(debug=True)
